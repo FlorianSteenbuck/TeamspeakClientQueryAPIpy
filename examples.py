bdfca37a69e19098712add2b742e87bc2e9aa30d
Florian Steenbuck <florian.steenbuck@floxen.de>
2015-08-25 09:45:50 +0200

    add close to end of example

--- examples.py
+++ examples.py
@@ -32,4 +32,5 @@ name = "I am Stupid"
 ts3query = api.teamspeak_socket_init()
 for i in range(2,len(name)+1):
 	print(api.teamspeak_socket_send(ts3query,"clientupdate client_nickname="+api.teamspeak_escape(api.an_write(i,name))))
+ts3query.close()
 input()
